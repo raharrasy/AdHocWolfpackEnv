Fix bugs related to predicted value computation in Agent.

Agent.py
@@ -655,8 +655,10 @@ class AdHocLearningAgent(Agent):
         self.hidden_u = list(zip([hid[None,None,:] for hid in u_hid[0][0]], [hid[None,None,:] for hid in u_hid[1][0]]))
 
         act = torch.argmax(out, dim=-1)
-        self.predicted_vals.append(out.gather(1, act[:,None]))
-        act = [a.item() if random.random() > self.epsilon else random.randint(0,6) for a in act]
+        act = [a.item() if random.random() > self.epsilon else
+                            random.randint(0, 6) for a in act]
+        self.predicted_vals.append(out.gather(1, torch.Tensor(act).long().to(self.device)[:,None]))
+        #act = [a.item() if random.random() > self.epsilon else random.randint(0,6) for a in act]
 
         return act
 
@@ -961,8 +963,9 @@ class AdHocShortBPTTAgent(Agent):
         self.hidden_u = list(zip([hid[None,None,:] for hid in u_hid[0][0]], [hid[None,None,:] for hid in u_hid[1][0]]))
 
         act = torch.argmax(out, dim=-1)
-        self.predicted_vals.append(out.gather(1, act[:,None]))
-        act = [a.item() if random.random() > self.epsilon else random.randint(0,6) for a in act]
+        act = [a.item() if random.random() > self.epsilon else
+               random.randint(0, 6) for a in act]
+        self.predicted_vals.append(out.gather(1, torch.Tensor(act).long().to(self.device)[:, None]))
 
         return act
 